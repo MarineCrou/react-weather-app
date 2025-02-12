5adf581dd889c8b4cecefbc8f9ecb2ee42c1d2ea
Marine Crouzet <marine.m.crouzet@gmail.com>
2025-02-12 18:45:35 +0100

    Designed the forecast display + added the weather icon matching each forecast date

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,22 +1,27 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
+import "./css/Forecast.css";
+import WeatherIcons from "./WeatherIcons";
+
 export default function Forecast({ city }) {
   const [forecastData, setForecastData] = useState({});
 
   useEffect(() => {
     if (city) {
-      callApi(success, error);
+      callApi();
     }
   }, [city]);
 
-  let success = (response) => {
+  let getForecast = (response) => {
+    console.log(response.data);
+
     const allForecasts = response.data.list;
-    const nineAmDate = allForecasts.filter((hour) => {
+    const nineAmData = allForecasts.filter((hour) => {
       return hour.dt_txt.includes("09:00:00");
     });
 
-    let daysOfTheWeek = nineAmDate.map((index) => {
+    let daysOfTheWeek = nineAmData.map((index) => {
       const unixSeconds = index.dt;
       const date = new Date(unixSeconds * 1000);
       const dayOfWeek = date.toLocaleDateString("en-US", {
@@ -25,35 +30,45 @@ export default function Forecast({ city }) {
       return dayOfWeek;
     });
 
-    let temperature = nineAmDate.map((index) => {
+    let temperature = nineAmData.map((index) => {
       const temp = Math.round(index.main.temp_max);
       return temp;
+    });
+
+    let weatherIcon = nineAmData.map((item) => {
+      const mainDescription = item.weather[0].main;
+      console.log(mainDescription);
+      return mainDescription;
     });
 
     let weather = {
       temp: temperature,
       weekDay: daysOfTheWeek,
+      icon: weatherIcon,
     };
     setForecastData(weather);
-  };
-
-  let error = () => {
-    console.log(`Could not retrieve city`);
   };
 
   const callApi = () => {
     const apiKey = "515c9ddbeb3cda9061acfab71031839e";
     const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(forecastUrl).then(success);
+    axios.get(forecastUrl).then(getForecast);
   };
+  console.log(forecastData.icon);
 
   return (
-    <div>
-      {forecastData.temp && forecastData.weekDay ? (
+    <div className="Forecast">
+      {forecastData.temp && forecastData.weekDay && forecastData.icon ? (
         forecastData.weekDay.map((day, index) => (
-          <p key={index}>
-            {day}: {forecastData.temp[index]}°C
-          </p>
+          <ul key={index}>
+            <li className="forecast-time">{day}</li>
+            <li className="weather-icon">
+              <WeatherIcons icon={forecastData.icon[index]} />
+            </li>
+            <li className="forecast-temperature">
+              {forecastData.temp[index]}°C
+            </li>
+          </ul>
         ))
       ) : (
         <p>Loading forecast...</p>
