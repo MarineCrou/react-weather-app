2a69c0e1e4b123255724907e78cd8fda65cffedb
Marine Crouzet <marine.m.crouzet@gmail.com>
2025-02-13 16:51:16 +0100

    added a new component, units, so users can change the current weather from metrics to imperial and back

--- src/Units.js
+++ src/Units.js
@@ -1,19 +1,16 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./css/Units.css";
 
 export default function Units({ unitTemp }) {
   console.log(unitTemp);
   const [unit, setUnit] = useState(unitTemp);
-  const [activeUnit, setActiveUnit] = useState("metric");
 
   const getMetric = (e) => {
     e.preventDefault();
-    setActiveUnit("metric");
     setUnit(unitTemp);
   };
   const getImperial = (e) => {
     e.preventDefault();
-    setActiveUnit("imperial");
     let imperial = Math.round(unitTemp * (9 / 5) + 32);
     setUnit(imperial);
   };
